c4831c34677537135ac61d19514f7600914dcbe8
RazAmram93 <razamram1993@gmail.com>
2023-12-11 00:27:07 +0200

    finished chapter 10

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+import DashboardSkeleton from "../../ui/skeletons";
+//This is the loading comp.
+//When fetching data the page will show the templets of the page without the info to show the client that the page is loading
+export default function Loading(){
+    return <DashboardSkeleton/>
+}
